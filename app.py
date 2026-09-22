25c5cfe6802219a3cffa079162066c2889f9dbd5
jeeva44431 <ssmc24bca161.ssmrv@rvei.edu.in>
2026-09-22 14:18:18 +0530

    Correct typos in Streamlit app
    Fix typos in number input prompt and button conditions.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-a=st.number_input("Enter a number")
+a=st.number_input("Enter a nuaamber")
 b=st.number_input("Enter another number")
 if-st.button("add"):
 	st.success(a+b)
